c5af0a9cc5492d049c65c2a7d5aa74b630fd230d
ChienYuYu <jianyou224@gmail.com>
2025-04-19 16:06:07 +0800

    0419

---
+++ 0419.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data=pd.read_csv('./Salary_Data.csv')
+x=data['YearsExperience']
+y=data['Salary']
+# print(x)
+# print('------------')
+# print(y)
